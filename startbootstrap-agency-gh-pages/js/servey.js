238640f6f527e99be6563d3eba388681a43fdc85
Chamath Kalhara <chamathkalhara93@gmail.com>
2016-12-03 16:15:42 +0530

    signup js stuf

---
+++ startbootstrap-agency-gh-pages/js/servey.js
@@ -0,0 +1,13 @@
+$('#reg').click(function () {
+     var username = $('#username').val();
+     var name = $('#name').val();
+     var mail = $('#mail').val();
+     var password = $('#password').val();
+
+     var selected = new Array();
+     $(".select").each(function(){
+     	selected.push($(this).val();
+  	});
+
+
+});
